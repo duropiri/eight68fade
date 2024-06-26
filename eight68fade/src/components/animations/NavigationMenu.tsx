e3b28129ab99599885f5f22d959d942a2a347bb1
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2024-06-26 14:16:38 -0600

    header

--- eight68fade/src/components/animations/NavigationMenu.tsx
+++ eight68fade/src/components/animations/NavigationMenu.tsx
@@ -222,7 +222,7 @@ const Header: React.FC<HeaderProps> = ({ navigation }) => {
         }`}
       >
         <div className="relative flex justify-between text-xs uppercase font-bold">
-          <div className="relative text-6xl uppercase font-extrabold">
+          <div className="relative text-4xl uppercase font-extrabold">
             eight68fade
             <AnimatePresence>
               <span className="text-sm">{getChars("")}</span>
